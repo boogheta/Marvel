353ac24c8f648f861e93c76c1883a7bc3df328a6
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-02-10 18:16:58 +0100

    handle comics in mongo since too many to handle as json

--- bin/assemble_jsons.py
+++ bin/assemble_jsons.py
@@ -2,16 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, re, json
+import pymongo
 
+re_data_name = re.compile(r'^.*/([^/]+)$')
 try:
     jsondir = sys.argv[1]
     assert(os.path.isdir(jsondir))
+    dataset = re_data_name.sub(r'\1', jsondir)
 except:
-    sys.stderr.write('Usage ./assemble_jsons.py <directory where the set of jsons are>\n')
+    sys.stderr.write('Usage ./assemble_jsons.py <directory where the set of jsons are> [<save_in_mongo instead of log output>]\n')
     sys.exit(1)
 
-res = []
 total = 0
+res = []
+try:
+    db = pymongo.Connection('localhost', 27017)["marvel"]
+    db.drop_collection(dataset)
+    coll = db[dataset]
+#    coll.ensure_index([('timestamp', pymongo.ASCENDING)], background=True)
+except Exception as e:
+    sys.stderr.write('ERROR: Could not initiate connection to MongoDB: %s %s\n' % (type(e), e))
+    sys.exit(1)
 
 re_match_file = re.compile(r'(^|%s)\d+.json$' % os.sep)
 for jsonfile in os.listdir(jsondir):
@@ -21,13 +32,22 @@ for jsonfile in os.listdir(jsondir):
         with open(os.path.join(jsondir, jsonfile)) as f:
             data = json.loads(f.read())['data']
     except Exception as e:
-        print type(e),e
-        sys.stderr.write("WARNING: Error reading file %s\n" % jsonfile)
+        sys.stderr.write("WARNING: Error reading file %s: %s %s\n" % (jsonfile, type(e), e))
         continue
     if total and total != data['total']:
         sys.stderr.write("WARNING: Different total values in different files: %s & %s\n" % (total, data['total']))
     total = data['total']
-    res += data['results']
+    if dataset != "comics":
+        res += data['results']
+    for d in data['results']:
+        d['_id'] = d['id']
+        coll.save(d)
+    del(data)
 
-print json.dumps(res, ensure_ascii=False).encode('utf8')
-sys.stderr.write(" -> Saved a total of %s\n" % len(res))
+if res:
+    print json.dumps(res, ensure_ascii=False).encode('utf8')
+    tot = len(res)
+else:
+    tot = total
+total = len(list(coll.find()))
+sys.stderr.write(" -> Saved a total of %s %s against supposedly %s\n" % (total, dataset, tot))
