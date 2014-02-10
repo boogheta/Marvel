4ac02a9b0de8ccf629d9930f7ef130ba6441a098
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-02-10 18:15:39 +0100

    allow to start downloader from a certain point to run concurrently

--- bin/get_dataset.sh
+++ bin/get_dataset.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 dataset=$1
+st=$2
+if [ -z $st ]; then
+  st=0
+fi
 
 source config
 ts=$(date +%s)
@@ -9,11 +13,13 @@ key="ts=$ts&apikey=$key&hash="$(echo -n "$ts$secret$key" | md5sum | awk '{print 
 echo "Work on $dataset" >&2
 echo "-----------" >&2
 mkdir -p data/$dataset
-i=0
-max=15
+i=$st
+max=$(($st+15))
 while [ $i -lt $max ]; do
   ct=0
-  echo " -> DL $dataset page $i" >&2
+  if [ ! -s data/$dataset/$i.json ]; then
+    echo " -> DL $dataset page $i" >&2
+  fi
   while [ $ct -lt 3 ] && [ ! -s data/$dataset/$i.json ]; do
     curl -f -s -L "http://gateway.marvel.com/v1/public/$dataset?$key&limit=100&orderBy=modified&offset="$(($i*100)) > data/$dataset/$i.json
     ct=$(($ct+1))
@@ -27,6 +33,8 @@ while [ $i -lt $max ]; do
   i=$(($i+1))
 done
 echo " -> Assemble all jsons into data/$dataset.json" >&2
-./bin/assemble_jsons.py data/$dataset > data/$dataset.json
+if [ $st -eq 0 ]; then
+  ./bin/assemble_jsons.py data/$dataset > data/$dataset.json
+fi
 echo >&2
 
