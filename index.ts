9d6830ef8a7048d21d3987a357c55ac65587774a
RouxRC <b.ooghe@gmail.com>
2022-09-18 23:07:48 +0200

    better artist/writer legend

--- index.ts
+++ index.ts
@@ -256,10 +256,15 @@ function loadNetwork() {
       });
       nodeExtra.innerHTML = "<p>" + attrs.description + "</p>";
       nodeExtra.innerHTML += "<p>Accounted in <b>" + attrs.comics + " stories</b> shared with <b>" + graph.degree(node) + " other " + entity + "</b></p>";
-      if (entity === "creators")
-        nodeExtra.innerHTML += "<p>Including <b>" + attrs.writer + " as writer</b> and <b>" + attrs.artist + " as artist</b></p>";
+      if (entity === "creators") {
+        if (attrs.writer === 0 && attrs.artist)
+          nodeExtra.innerHTML += '<p>Always as <b style="color: ' + clusters.roles.artist + '">artist</b></p>';
+        else if (attrs.artist === 0 && attrs.writer)
+          nodeExtra.innerHTML += '<p>Always as <b style="color: ' + clusters.roles.writer + '">writer</b></p>';
+        else nodeExtra.innerHTML += '<p>Including <b style="color: ' + clusters.roles.writer + '">' + attrs.writer + ' as writer</b> and <b style="color: ' + clusters.roles.artist + '">' + attrs.artist + " as artist</b></p>";
+      }
       if (clusters.communities[communities[node]])
-        nodeExtra.innerHTML += '<p>Attached to the <b><span style="color: ' + clusters.communities[communities[node]].color + '">' + clusters.communities[communities[node]].cluster + '</span></b> community<sup>*</sup></p>';
+        nodeExtra.innerHTML += '<p>Attached to the <b style="color: ' + clusters.communities[communities[node]].color + '">' + clusters.communities[communities[node]].cluster + '</b> community<sup>*</sup></p>';
       if (attrs.url)
         nodeExtra.innerHTML += '<p><a href="' + attrs.url + '" target="_blank">More on Marvel.com…</a></p>';
 
