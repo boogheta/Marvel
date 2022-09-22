6ca617d4761bd37086d491f2771783dc7b733c9c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2022-09-22 19:07:31 +0200

    better handle node sizes on zoom

--- index.ts
+++ index.ts
@@ -148,15 +148,14 @@ const defaultSidebar = function() {
 
 let graph = null,
   renderer = null,
-  camera = null,
-  ratio = 1;
+  camera = null;
 
-const computeNodeSize = function(node, stories, sigmaDim) {
+const computeNodeSize = function(node, stories, sigmaDim, ratio) {
   return Math.pow(stories, 0.2)
-    * (entity == "characters" ? 2 : 1.25)
-    * (network_size === "small" ? 2 : 1.25)
+    * (entity == "characters" ? 1.75 : 1.25)
+    * (network_size === "small" ? 1.75 : 1.25)
     * sigmaDim / 1000
-    * ratio;
+    / ratio;
 };
 
 function loadNetwork() {
@@ -194,7 +193,7 @@ function loadNetwork() {
     graph.forEachNode((node, {x, y,stories, thumbnail, artist, writer, community}) => {
       const artist_ratio = (entity === "creators" ? artist / (writer + artist) : undefined),
         sigmaDim = Math.min(divHeight("sigma-container"), divWidth("sigma-container"));
-      graph.setNodeAttribute(node, "size", computeNodeSize(node, stories, sigmaDim));
+      graph.setNodeAttribute(node, "size", computeNodeSize(node, stories, sigmaDim, 1));
       spatializedPositions[node] = {x: x, y: y};
       graph.mergeNodeAttributes(node, {
         x: circularPositions[node].x,
@@ -230,42 +229,33 @@ function loadNetwork() {
 
     if (view === "colors") switchView();
 
-    const adjustNodesSizeToZoom = function() {
-      const sigmaDim = Math.min(divHeight("sigma-container"), divWidth("sigma-container")),
-        newSizes = {};
-      graph.forEachNode((node, {stories}) => {
-        newSizes[node] = {size: computeNodeSize(node, stories, sigmaDim)}
-      });
-      animateNodes(graph, newSizes, { duration: 400, easing: "quadraticOut" });
-    }
     // Bind zoom manipulation buttons
+    const adjustNodesSizeToZoom = function(extraRatio) {
+      const sigmaDim = Math.min(divHeight("sigma-container"), divWidth("sigma-container")),
+        newSizes = {},
+        ratio = extraRatio ? Math.pow(1.1, Math.log(camera.ratio * extraRatio) / Math.log(1.5)) : 1;
+      graph.forEachNode((node, {stories}) => {
+        newSizes[node] = {size: computeNodeSize(node, stories, sigmaDim, ratio)}
+      });
+      animateNodes(graph, newSizes, { duration: extraRatio == 1 ? 200 : 600, easing: "quadraticOut" });
+    }
     camera = renderer.getCamera();
     document.getElementById("zoom-in").addEventListener("click", () => {
       camera.animatedZoom({ duration: 600 });
       if (camera.ratio > sigmaSettings.minCameraRatio)
-        ratio *= 1.1;
-      adjustNodesSizeToZoom();
+        adjustNodesSizeToZoom(1/1.5);
     });
     document.getElementById("zoom-out").addEventListener("click", () => {
       camera.animatedUnzoom({ duration: 600 });
       if (camera.ratio < sigmaSettings.maxCameraRatio)
-        ratio /= 1.1;
-      adjustNodesSizeToZoom();
+        adjustNodesSizeToZoom(1.5);
     });
     document.getElementById("zoom-reset").addEventListener("click", () => {
       camera.animatedReset({ duration: 600 });
-      ratio = 1;
-      adjustNodesSizeToZoom();
+      adjustNodesSizeToZoom(0);
     });
     const handleWheel = function(e) {
-      if (e.event.delta > 0) {
-        if (camera.ratio > sigmaSettings.minCameraRatio)
-          ratio *= 1.1;
-      } else if (e.event.delta < 0) {
-        if (camera.ratio < sigmaSettings.maxCameraRatio)
-          ratio /= 1.1;
-      } else return;
-      adjustNodesSizeToZoom();
+      setTimeout(() => adjustNodesSizeToZoom(1), 200);
     };
     renderer.on("wheelNode", (e) => handleWheel(e));
     renderer.on("wheelEdge", (e) => handleWheel(e));
@@ -317,13 +307,13 @@ function loadNetwork() {
           if (view === "pictures")
             return (n === node || graph.hasEdge(n, node)
               ? { ...data, zIndex: 1, size: data.size * (n === node ? 1.5 : 1) }
-              : { ...data, zIndex: 0, color: "#2A2A2A", image: null, size: network_size === "small" ? 5 : 3 }
+              : { ...data, zIndex: 0, color: "#2A2A2A", image: null, size: network_size === "small" ? 4 : 2 }
             )
           else return (n === node
-            ? { ...data, zIndex: 1, size: data.size * (n === node ? 1.5 : 1) }
+            ? { ...data, zIndex: 2, size: data.size * 1.5 }
             : (graph.hasEdge(n, node)
-                ? { ...data, zIndex: 1, size: data.size * (n === node ? 1.5 : 1), image: null }
-                : { ...data, zIndex: 0, color: "#2A2A2A", image: null, size: network_size === "small" ? 5 : 3 }
+                ? { ...data, zIndex: 1, image: null }
+                : { ...data, zIndex: 0, color: "#2A2A2A", image: null, size: network_size === "small" ? 4 : 2 }
               )
             )
         }
@@ -483,10 +473,11 @@ function resize() {
     explanations.style["min-height"] = (freeHeight - 13) + "px";
     nodeDetails.style.height = (freeHeight - 18) + "px";
     nodeDetails.style["min-height"] = (freeHeight - 18) + "px";
-    if (graph) {
-      const sigmaDim = Math.min(divHeight("sigma-container"), divWidth("sigma-container"));
+    if (graph && camera) {
+      const sigmaDim = Math.min(divHeight("sigma-container"), divWidth("sigma-container")),
+        ratio = Math.pow(1.1, Math.log(camera.ratio) / Math.log(1.5));
       graph.forEachNode((node, {stories}) =>
-        graph.setNodeAttribute(node, "size", computeNodeSize(node, stories, sigmaDim))
+        graph.setNodeAttribute(node, "size", computeNodeSize(node, stories, sigmaDim, ratio))
       );
     }
     resizing = false;
