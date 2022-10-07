19855af2eb09b722d5147f5d679d47789626e8d6
RouxRC <b.ooghe@gmail.com>
2022-10-07 03:16:29 +0000

    Deploying to gh-pages from  @ f5183d4a2fa304750bf7391fd9c7a8a0f28de293 🚀

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 /* TODO:
+- bug unzoom box smartphone
+- bug resize on smartphone
 - bind url with selected comic?
 - display creators/characters by comic (with link actions?)
 - button Explore All comics
@@ -563,7 +565,7 @@ function selectComic(comic = null, keep = false) {
         ? { ...attrs,
             zIndex: 0,
             color: '#333',
-            size: 2
+            size: 3
           }
         : { ...attrs,
             zIndex: 0,
@@ -935,7 +937,7 @@ function clickNode(node, updateURL=true) {
         ? { ...attrs,
             zIndex: 0,
             color: lighten(data.graph.getNodeAttribute(data.graph.opposite(node, edge), 'color'), 75),
-            size: Math.max(0.1, Math.log(data.graph.getEdgeAttribute(edge, 'weight') * sigmaDim / 200000))
+            size: Math.max(0.5, Math.log(data.graph.getEdgeAttribute(edge, 'weight') * sigmaDim / 300000))
           }
         : { ...attrs,
             zIndex: 0,
